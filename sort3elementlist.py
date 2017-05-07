702d90345eea904a59547a7e42a801e459dde420
konradzicho <konradzicho@gmail.com>
2017-05-08 01:45:49 +0200

    Update sort3elementlist.py

--- sort3elementlist.py
+++ sort3elementlist.py
@@ -3,9 +3,16 @@ def swap(lista, index1, index2):
   lista[index1] = lista[index2]
   lista[index2] = buffer
   return lista
-  
-print(swap([2, 7], 0, 1))
 
-print([7, 2] == swap([2, 7], 0, 1))
-print([7, 2] == swap([2, 7], 1, 0))
-print([5, 11, 7] == swap([5, 7, 11], 1, 2))
+def sort3elementList(lista):
+  if (lista[0]>lista[1]):
+    swap (lista, 0, 1)
+  if (lista[1]>lista[2]):
+    swap (lista, 1, 2)
+  if (lista[0]>lista[1]):
+    swap (lista, 0, 1)
+  return lista
+    
+print([2, 5, 7] == sort3elementList([5, 2, 7]))
+print([2, 5, 7] == sort3elementList([5, 7, 2]))
+print([2, 5, 7] == sort3elementList([7, 2, 5]))
